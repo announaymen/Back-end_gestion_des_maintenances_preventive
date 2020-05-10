a392118237d7f294201dae6ab2484e495d163509
announaymen <a.announ@esi-sba.dz>
2020-05-10 03:02:31 +0100

    d

---
+++ node.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./queries");
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// routes
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+app.post("/employees", db.createEmployee);
+app.post("/auth", db.auth);
+app.get("/employees", db.getEmployees);
+app.get("/employees/:id", db.getEmployeeById);
+app.put("/employees/:id", db.updateEmployee);
+app.delete("/employees/:id", db.deleteEmployee);
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
